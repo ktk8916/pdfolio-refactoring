623e6e13e70df240c3e2c9b8c263e472d5e5db6c
ktk8916 <ktk8916@naver.com>
2023-11-02 23:20:08 +0900

    회원가입 기능 테스트

---
+++ src/test/java/com/playdata/pdfolio/member/domain/entity/MemberTest.java
@@ -0,0 +1,36 @@
+package com.playdata.pdfolio.member.domain.entity;
+
+import com.playdata.pdfolio.global.type.SkillType;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MemberTest {
+
+    @DisplayName("회원가입 메서드로 멤버의 값을 변경한다.")
+    @Test
+    void signup(){
+        // given
+        Member member = Member.fromId(1L);
+
+        // when
+        member.signup(
+                "nick",
+                "www.cdn.com",
+                List.of(MemberSkill.builder().skillType(SkillType.JAVA).build(),
+                        MemberSkill.builder().skillType(SkillType.SPRING).build()
+                )
+        );
+
+        // then
+        assertThat(member.getNickname()).isEqualTo("nick");
+        assertThat(member.getImageUrl()).isEqualTo("www.cdn.com");
+        assertThat(member.getSkills())
+                .extracting("skillType")
+                .containsExactlyInAnyOrder(SkillType.JAVA, SkillType.SPRING);
+    }
+
+}
