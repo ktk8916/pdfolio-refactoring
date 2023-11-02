3ff5ae78c842594cac978bbcab4fa836fc1e4727
ktk8916 <ktk8916@naver.com>
2023-11-02 18:29:15 +0900

    이름 변경

--- src/main/java/com/playdata/pdfolio/oauth2/domain/entity/Oauth2TokenDto.java
+++ src/main/java/com/playdata/pdfolio/oauth2/domain/entity/Oauth2TokenDto.java
@@ -3,7 +3,7 @@ package com.playdata.pdfolio.oauth2.domain.entity;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 
-public record Oauth2AccessToken(
+public record Oauth2TokenDto(
         @JsonProperty("access_token") String token,
         @JsonProperty("token_type") String type,
         String scope) {
