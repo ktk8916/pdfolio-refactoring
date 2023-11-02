f3fedd12aed1676c6a842d6186c9726607cbaeef
ktk8916 <ktk8916@naver.com>
2023-11-02 20:44:07 +0900

    변수명 변경 providerName -> provider

--- src/main/java/com/playdata/pdfolio/oauth2/client/Oauth2Client.java
+++ src/main/java/com/playdata/pdfolio/oauth2/client/Oauth2Client.java
@@ -19,53 +19,53 @@ import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
 public class Oauth2Client {
 
     private final ProviderFactory providerFactory;
 
-    public Oauth2UserInfo authenticate(String providerName, String code) {
-        Oauth2AccessToken accessToken = getAccessToken(providerName, code);
-        return getUserInfo(providerName, accessToken.token());
+    public Oauth2UserInfo authenticate(String provider, String code) {
+        Oauth2AccessToken accessToken = getAccessToken(provider, code);
+        return getUserInfo(provider, accessToken.token());
     }
 
-    private Oauth2AccessToken getAccessToken(String providerName, String code) {
-        Oauth2Provider provider = providerFactory.getProvider(providerName);
+    private Oauth2AccessToken getAccessToken(String provider, String code) {
+        Oauth2Provider oauth2provider = providerFactory.getProvider(provider);
+
         return WebClient.create()
                 .post()
-                .uri(provider.getAccessTokenUri())
+                .uri(oauth2provider.getAccessTokenUri())
                 .headers(header -> {
                     header.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
                     header.setAccept(Collections.singletonList(MediaType.APPLICATION_JSON));
                     header.setAcceptCharset(Collections.singletonList(StandardCharsets.UTF_8));
                 })
-                .bodyValue(tokenRequest(code, provider))
+                .bodyValue(tokenRequest(code, oauth2provider))
                 .retrieve()
                 .bodyToMono(Oauth2AccessToken.class)
                 .block();
     }
 
-    private Oauth2UserInfo getUserInfo(String providerName, String accessToken) {
-        Oauth2Provider provider = providerFactory.getProvider(providerName);
+    private Oauth2UserInfo getUserInfo(String provider, String accessToken) {
+        Oauth2Provider oauth2provider = providerFactory.getProvider(provider);
+
         Map<String, Object> attributes = WebClient.create()
                 .get()
-                .uri(provider.getUserInfoUri())
+                .uri(oauth2provider.getUserInfoUri())
                 .headers(header -> header.setBearerAuth(accessToken))
                 .retrieve()
-                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {
-                })
+                .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
                 .block();
 
-        return Oauth2UserInfo.of(provider.getName(), attributes);
+        return Oauth2UserInfo.of(oauth2provider.getName(), attributes);
     }
 
-    private MultiValueMap<String, String> tokenRequest(String code, Oauth2Provider provider) {
+    private MultiValueMap<String, String> tokenRequest(String code, Oauth2Provider oauth2provider) {
         MultiValueMap<String, String> formData = new LinkedMultiValueMap<>();
         formData.add("code", code);
-        formData.add("client_id", provider.getClientId());
-        formData.add("client_secret", provider.getClientSecret());
+        formData.add("client_id", oauth2provider.getClientId());
+        formData.add("client_secret", oauth2provider.getClientSecret());
         formData.add("grant_type", "authorization_code");
-        formData.add("redirect_uri", provider.getRedirectUri());
+        formData.add("redirect_uri", oauth2provider.getRedirectUri());
         return formData;
     }
 }
