6bd5fab102b55de4d3d2ebb364e10803a154dc05
ktk8916 <ktk8916@naver.com>
2023-11-02 21:43:21 +0900

    테스트 프로파일 추가

--- src/test/java/com/playdata/pdfolio/PdfolioApplicationTests.java
+++ src/test/java/com/playdata/pdfolio/PdfolioApplicationTests.java
@@ -2,8 +2,10 @@ package com.playdata.pdfolio;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PdfolioApplicationTests {
 
     @Test
