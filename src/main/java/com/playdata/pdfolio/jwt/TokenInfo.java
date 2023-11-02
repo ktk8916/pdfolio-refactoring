a464c6342d605d49252ae704a8776938d95f4408
ktk8916 <ktk8916@naver.com>
2023-11-02 21:43:00 +0900

    getter 추가

--- src/main/java/com/playdata/pdfolio/jwt/TokenInfo.java
+++ src/main/java/com/playdata/pdfolio/jwt/TokenInfo.java
@@ -1,11 +1,13 @@
 package com.playdata.pdfolio.jwt;
 
 import lombok.Builder;
+import lombok.Getter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
+@Getter
 public class TokenInfo implements UserDetails {
 
     private Long id;
