8da1370b93363ba958f5d940144d6ad227fba70f
ktk8916 <ktk8916@naver.com>
2023-11-08 00:37:27 +0900

    gather 예외 추가

--- src/main/java/com/playdata/pdfolio/global/exception/ExceptionType.java
+++ src/main/java/com/playdata/pdfolio/global/exception/ExceptionType.java
@@ -1,5 +1,8 @@
 package com.playdata.pdfolio.global.exception;
 
+import com.playdata.pdfolio.gather.exception.GatherNotFoundException;
+import com.playdata.pdfolio.gather.exception.InvalidGatherDurationException;
+import com.playdata.pdfolio.gather.exception.InvalidGatherWriterException;
 import com.playdata.pdfolio.member.exception.MemberNotFoundException;
 import com.playdata.pdfolio.member.exception.UnregisteredMemberException;
 import com.playdata.pdfolio.oauth2.exception.NotSupportedOauth2Exception;
@@ -23,6 +26,10 @@ public enum ExceptionType {
 
     UNREGISTERED_MEMBER(HttpStatus.ACCEPTED, "가입되지 않은 멤버입니다.", UnregisteredMemberException.class),
     MEMBER_NOT_FOUND(HttpStatus.BAD_REQUEST, "멤버를 찾을 수 없습니댜.", MemberNotFoundException.class),
+
+    INVALID_GATHER_DURATION(HttpStatus.BAD_REQUEST, "유효하지 않은 모집기간 입니다.", InvalidGatherDurationException.class),
+    GATHER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 모집글입니다.", GatherNotFoundException.class),
+    INVALID_GATHER_WRITER(HttpStatus.FORBIDDEN, "유효하지 않은 모집글 작성자입니다.", InvalidGatherWriterException.class),
 
     INVALID_PROJECT_SEARCH_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 프로젝트 검색 타입입니다.",InValidProjectSearchTypeException .class),
     INVALID_PAGE_EXCEPTION(HttpStatus.BAD_REQUEST, "유효하지 않은 페이지입니다.", InvalidPageException.class),
