cd42f91f3d1d9709e36fd32df26d04a7eb8914f5
ktk8916 <ktk8916@naver.com>
2023-11-08 00:33:21 +0900

    요청 인증 설정

--- src/main/java/com/playdata/pdfolio/global/config/SecurityConfig.java
+++ src/main/java/com/playdata/pdfolio/global/config/SecurityConfig.java
@@ -5,8 +5,10 @@ import com.playdata.pdfolio.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,18 +19,20 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtProvider jwtProvider;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .httpBasic(httpBasic -> httpBasic.disable())
-                .csrf(csrf -> csrf.disable())
-                .formLogin(formLogin -> formLogin.disable())
-                .sessionManagement(sessionManagement->
-                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(sessionManagement-> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(
                         authorizeRequests -> authorizeRequests
-//                                .requestMatchers("/api/v1/oauth2/*").permitAll()
-                                .anyRequest().permitAll()
+                                .requestMatchers(HttpMethod.POST,"/api/v1/auth/*").permitAll()
+                                .requestMatchers(HttpMethod.GET,"/api/v1/oauth2/*").permitAll()
+                                .requestMatchers(HttpMethod.GET,"/api/v1/project/*").permitAll()
+                                .requestMatchers(HttpMethod.GET,"/api/v1/gather/*").permitAll()
+                                .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter.class);
 
