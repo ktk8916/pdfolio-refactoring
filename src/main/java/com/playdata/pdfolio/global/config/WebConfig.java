01feadc872f90fa0e48a11c74e498837c0e13044
ktk8916 <ktk8916@naver.com>
2023-11-08 00:32:59 +0900

    필요없는 생성자 제거

--- src/main/java/com/playdata/pdfolio/global/config/WebConfig.java
+++ src/main/java/com/playdata/pdfolio/global/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.playdata.pdfolio.global.config;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,7 +7,6 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
