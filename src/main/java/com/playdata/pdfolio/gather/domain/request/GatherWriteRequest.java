6e3af56b389a8d6008e072157a7817c3c0340bfa
ktk8916 <ktk8916@naver.com>
2023-11-07 21:45:41 +0900

    모집글 작성

--- src/main/java/com/playdata/pdfolio/gather/domain/request/GatherWriteRequest.java
+++ src/main/java/com/playdata/pdfolio/gather/domain/request/GatherWriteRequest.java
@@ -3,19 +3,17 @@ package com.playdata.pdfolio.gather.domain.request;
 import com.playdata.pdfolio.gather.domain.entity.Gather;
 import com.playdata.pdfolio.gather.domain.entity.GatherCategory;
 import com.playdata.pdfolio.member.domain.entity.Member;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
-public record WriteRequest(
+public record GatherWriteRequest(
         String title,
         String content,
         LocalDate startDate,
         LocalDate closeDate,
-        Long teamSize,
-        @Enumerated(EnumType.STRING)
+        int teamSize,
         GatherCategory category,
         String contact,
         List<String> skills
@@ -29,6 +27,7 @@ public record WriteRequest(
                 .startDate(startDate)
                 .closeDate(closeDate)
                 .category(category)
+                .skills(new ArrayList<>())
                 .contact(contact)
                 .build();
     }
