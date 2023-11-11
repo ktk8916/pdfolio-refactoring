f9cd4a08c997be904caea426f3e53ebfd49f1a86
ktk8916 <ktk8916@naver.com>
2023-11-11 21:22:21 +0900

    설정파일로 정보 이동

--- src/main/java/com/playdata/pdfolio/global/config/WebConfig.java
+++ src/main/java/com/playdata/pdfolio/global/config/WebConfig.java
@@ -1,21 +1,27 @@
 package com.playdata.pdfolio.global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${cors.allow-origin}")
+    private String allowOrigin;
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry
                 .addMapping("/**")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH")
-                .allowedOrigins("http://localhost:3000");
-
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name())
+                .allowedOrigins(allowOrigin);
     }
 }
 
