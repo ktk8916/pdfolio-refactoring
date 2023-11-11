0873d193f358aac9c7d4ee3ea3b36a4ae192b677
ktk8916 <ktk8916@naver.com>
2023-11-11 22:51:23 +0900

    모집글 댓글 예외

---
+++ src/main/java/com/playdata/pdfolio/gather/exception/InvalidGatherCommentWriterException.java
@@ -0,0 +1,6 @@
+package com.playdata.pdfolio.gather.exception;
+
+import com.playdata.pdfolio.global.exception.PdFolioException;
+
+public class InvalidGatherCommentWriterException extends PdFolioException {
+}
--- src/main/java/com/playdata/pdfolio/global/exception/ExceptionType.java
+++ src/main/java/com/playdata/pdfolio/global/exception/ExceptionType.java
@@ -1,9 +1,6 @@
 package com.playdata.pdfolio.global.exception;
 
-import com.playdata.pdfolio.gather.exception.DeletedGatherException;
-import com.playdata.pdfolio.gather.exception.GatherNotFoundException;
-import com.playdata.pdfolio.gather.exception.InvalidGatherDurationException;
-import com.playdata.pdfolio.gather.exception.InvalidGatherWriterException;
+import com.playdata.pdfolio.gather.exception.*;
 import com.playdata.pdfolio.member.exception.MemberNotFoundException;
 import com.playdata.pdfolio.member.exception.UnregisteredMemberException;
 import com.playdata.pdfolio.oauth2.exception.NotSupportedOauth2Exception;
@@ -32,6 +29,9 @@ public enum ExceptionType {
     GATHER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 모집글입니다.", GatherNotFoundException.class),
     DELETED_GATHER(HttpStatus.NOT_FOUND, "삭제된 모집글입니다.", DeletedGatherException.class),
     INVALID_GATHER_WRITER(HttpStatus.FORBIDDEN, "유효하지 않은 모집글 작성자입니다.", InvalidGatherWriterException.class),
+
+    INVALID_GATHER_COMMENT_WRITER(HttpStatus.FORBIDDEN, "유효하지 않은 모집글 댓글 작성자입니다.", InvalidGatherCommentWriterException.class),
+    GATHER_COMMENT_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 모집글 댓글입니다.", GatherCommentNotFoundException.class),
 
     INVALID_PROJECT_SEARCH_TYPE(HttpStatus.BAD_REQUEST, "유효하지 않은 프로젝트 검색 타입입니다.",InValidProjectSearchTypeException .class),
     INVALID_PAGE_EXCEPTION(HttpStatus.BAD_REQUEST, "유효하지 않은 페이지입니다.", InvalidPageException.class),
