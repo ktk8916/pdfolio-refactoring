9a6f9b6eef36c5bde764f039ca1435f214b58174
ktk8916 <ktk8916@naver.com>
2023-11-26 21:03:10 +0900

    패지키 변경

--- src/main/java/com/playdata/pdfolio/gather/domain/request/GatherEditRequest.java
+++ src/main/java/com/playdata/pdfolio/gather/domain/request/GatherEditRequest.java
@@ -1,11 +1,8 @@
 package com.playdata.pdfolio.gather.domain.request;
 
-import com.playdata.pdfolio.gather.domain.entity.Gather;
 import com.playdata.pdfolio.gather.domain.entity.GatherCategory;
-import com.playdata.pdfolio.member.domain.entity.Member;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 
 public record GatherEditRequest(
